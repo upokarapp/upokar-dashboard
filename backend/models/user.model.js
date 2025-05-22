c43a9213beef7e64956a52ba7687f69af8903b4c
upokarapp <upokar697@gmail.com>
2025-05-22 07:43:26 +0600

    Update user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, unique: true },
+  email: { type: String },
   number: { type: String, required: true, unique: true },
   address: { type: String, required: true },
   password: { type: String, required: true },
